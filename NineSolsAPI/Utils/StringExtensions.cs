adb8af93e7a1b0c0608ce14af869e56d9d12b161
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-07-18 17:37:40 +0200

    more string extensions

--- NineSolsAPI/Utils/StringExtensions.cs
+++ NineSolsAPI/Utils/StringExtensions.cs
@@ -8,4 +8,20 @@ public static class StringExtensions {
         var idx = span.LastIndexOf(substr);
         return idx == -1 ? span : span[..idx];
     }
+
+    public static ReadOnlySpan<char> TrimEndMatches(this ReadOnlySpan<char> span, ReadOnlySpan<char> substr) {
+        var idx = span.LastIndexOf(substr);
+        return idx == -1 ? span : span[..idx];
+    }
+
+    public static ReadOnlySpan<char> TrimStartMatches(this string str, ReadOnlySpan<char> substr) {
+        var span = str.AsSpan();
+        var idx = span.LastIndexOf(substr);
+        return idx == -1 ? span : span[(idx + substr.Length) ..];
+    }
+
+    public static ReadOnlySpan<char> TrimStartMatches(this ReadOnlySpan<char> span, ReadOnlySpan<char> substr) {
+        var idx = span.LastIndexOf(substr);
+        return idx == -1 ? span : span[(idx + substr.Length) ..];
+    }
 }
