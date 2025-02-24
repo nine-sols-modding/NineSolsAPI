86d8c9fe02d4b89e6b8be424a5c3b44e6ef62d92
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-02-24 22:04:38 +0100

    versioncompat: Application.LoadScene

--- NineSolsAPI/VersionCompatExtensions.cs
+++ NineSolsAPI/VersionCompatExtensions.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Reflection;
+using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 
 namespace DebugModPlus;
 
 [PublicAPI]
 public static class VersionCompatExtensions {
-    private static MethodBase? changeScene2 = typeof(GameCore).GetMethod("ChangeScene",
+    private static MethodBase? gameCoreChangeScene2 = typeof(GameCore).GetMethod("ChangeScene",
         [typeof(SceneConnectionPoint.ChangeSceneData), typeof(bool), typeof(bool)]);
 
-    private static MethodBase? changeScene3 = typeof(GameCore).GetMethod("ChangeScene",
+    private static MethodBase? gameCoreChangeScene3 = typeof(GameCore).GetMethod("ChangeScene",
         [typeof(SceneConnectionPoint.ChangeSceneData), typeof(bool), typeof(bool), typeof(float)]);
 
     /**
@@ -17,12 +18,33 @@ public static class VersionCompatExtensions {
      */
     public static void ChangeSceneCompat(this GameCore gameCore, SceneConnectionPoint.ChangeSceneData changeSceneData,
         bool showTip, bool captureLastImage = false, float delayTime = 0) {
-        if (changeScene2 != null) {
-            changeScene2.Invoke(gameCore, [changeSceneData, showTip, captureLastImage]);
-        } else if (changeScene3 != null) {
-            changeScene3.Invoke(gameCore, [changeSceneData, showTip, captureLastImage, delayTime]);
+        if (gameCoreChangeScene2 != null) {
+            gameCoreChangeScene2.Invoke(gameCore, [changeSceneData, showTip, captureLastImage]);
+        } else if (gameCoreChangeScene3 != null) {
+            gameCoreChangeScene3.Invoke(gameCore, [changeSceneData, showTip, captureLastImage, delayTime]);
         } else {
             throw new Exception("No candidate for GameCore.ChangeScene found");
         }
+    }
+
+    private static MethodBase? applicationCoreChangeScene = typeof(ApplicationCore).GetMethod("ChangeScene");
+
+    private static MethodBase? applicationCoreChangeSceneClean = typeof(ApplicationCore).GetMethod("ChangeSceneClean");
+
+    /**
+     * Replacement for `ApplicationCore.ChangeScene[Clean]` which works on all game versions.
+     */
+    public static UniTask ChangeSceneCompat(this ApplicationCore applicationCore, string sceneName,
+        bool showTip = true, bool showLoading = false, bool forceSpawnFromSavePoint = false, int savePointIndex = 0) {
+        object[] args = [sceneName, showTip, showLoading, forceSpawnFromSavePoint, savePointIndex];
+        if (applicationCoreChangeScene != null) {
+            return (UniTask)applicationCoreChangeScene.Invoke(applicationCore, args);
+        }
+
+        if (applicationCoreChangeSceneClean != null) {
+            return (UniTask)applicationCoreChangeSceneClean.Invoke(applicationCore, args);
+        }
+
+        throw new Exception("No candidate for ApplicationCode.ChangeScene[Clean] found");
     }
 }
