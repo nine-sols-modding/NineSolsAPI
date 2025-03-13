1e6b6f750234180f8e36d8da967bd363ff75352e
Jakob Hellermann <jakob.hellermann@protonmail.com>
2025-03-13 09:27:56 +0100

    fix TrimStartMatches/TrimEndMatches

--- NineSolsAPI/Utils/StringExtensions.cs
+++ NineSolsAPI/Utils/StringExtensions.cs
@@ -6,7 +6,7 @@ public static class StringExtensions {
     public static ReadOnlySpan<char> TrimEndMatches(this string str, ReadOnlySpan<char> substr) {
         var span = str.AsSpan();
         var idx = span.LastIndexOf(substr);
-        return idx == -1 ? span : span[..idx];
+        return idx == str.Length - substr.Length ? span[..idx] : span;
     }
 
     public static ReadOnlySpan<char> TrimEndMatches(this ReadOnlySpan<char> span, ReadOnlySpan<char> substr) {
@@ -22,13 +22,13 @@ public static class StringExtensions {
 
     public static ReadOnlySpan<char> TrimStartMatches(this ReadOnlySpan<char> span, ReadOnlySpan<char> substr) {
         var idx = span.LastIndexOf(substr);
-        return idx == -1 ? span : span[(idx + substr.Length) ..];
+        return idx == 0 ? span[substr.Length..] : span;
     }
 
     public static (string, string)? SplitOnce(this string str, char sep) {
         var i = str.LastIndexOf(sep);
-        var objectPath = str[..i];
-        var componentName = str[(i + 1)..];
-        return (objectPath, componentName);
+        var a = str[..i];
+        var b = str[(i + 1)..];
+        return (a, b);
     }
 }
